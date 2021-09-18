5ff9d668bbdeaad662b18810a9006f54d26a7acf
nefirg <gue15004@byui.edu>
2021-09-18 13:44:37 -0600

    Home Page

---
+++ java.js
@@ -0,0 +1,29 @@
+/*var string = document.lastModified;
+alert(document.lastModified);
+document.getElementById("currentdate").innertHTML = string;*/
+/*let daynames = [
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday"];
+let months = [
+    "January", 
+    "February",  
+    "March", 
+    "April", 
+    "May", 
+    "June", 
+    "July", 
+    "August", 
+    "September", 
+    "October", 
+    "November", 
+    "December"];*/
+let d = new Date();
+
+let fulldate = d.getMonth() + '/' + d.getDay() + '/' + d.getFullYear() + ' ';
+let fulltime = d.getHours() + ':' + d.getMinutes() + ':' + d.getSeconds();
+let both = fulldate + ' ' + fulltime;
+    document.getElementById("currentdate").innerHTML = d.toLocaleTimeString();
