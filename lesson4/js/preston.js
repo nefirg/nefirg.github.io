f16fbac05dabd69fe2a1aad00113a44b5d44909e
nefirg <gue15004@byui.edu>
2021-10-08 12:08:16 -0600

    Updated 10/08/2021

--- lesson4/js/preston.js
+++ lesson4/js/preston.js
@@ -1,10 +1,15 @@
 window.addEventListener('load', () => {
     let d = new Date();
 
-    let fulldate = d.getMonth() + '/' + d.getDay() + '/' + d.getFullYear() + ' ';
-    let fulltime = d.getHours() + ':' + d.getMinutes() + ':' + d.getSeconds();
-    let both = fulldate + ' ' + fulltime;
-    document.getElementById("currentdate").innerHTML = both;
+    let myDays = ["Sunday", "Monday", "Tuesday", "Wednesday",
+    "Thursday", "Friday", "Saturday"];
+    let myMonths = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    let thisDay = myDays[d.getDay()];
+    let thisMonth = myMonths[d.getMonth()];
+    let fulldate = thisDay + ', ' + d.getDay() + ' ' + thisMonth + ' ' + d.getFullYear();
+    // let fulltime = d.getHours() + ':' + d.getMinutes() + ':' + d.getSeconds();
+    // let both = fulldate + fulltime;
+    document.getElementById("currentdate").innerHTML = fulldate;
 })
 
 function toggleMenu() {
