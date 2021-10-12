9703d8496431d1db594348f7d4a41b38a761f3c2
nefirg <gue15004@byui.edu>
2021-10-11 18:08:00 -0600

    updated

--- lesson4/js/preston.js
+++ lesson4/js/preston.js
@@ -1,12 +1,12 @@
 window.addEventListener('load', () => {
     let d = new Date();
-
     let myDays = ["Sunday", "Monday", "Tuesday", "Wednesday",
     "Thursday", "Friday", "Saturday"];
     let myMonths = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
     let thisDay = myDays[d.getDay()];
     let thisMonth = myMonths[d.getMonth()];
-    let fulldate = thisDay + ', ' + d.getDay() + ' ' + thisMonth + ' ' + d.getFullYear();
+    
+    let fulldate = thisDay + ', ' + d.getDate() + ' ' + thisMonth + ' ' + d.getFullYear();
     // let fulltime = d.getHours() + ':' + d.getMinutes() + ':' + d.getSeconds();
     // let both = fulldate + fulltime;
     document.getElementById("currentdate").innerHTML = fulldate;
