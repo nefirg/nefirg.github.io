8432787ec0a165750a759b18c3612fc510afe170
nefirg <gue15004@byui.edu>
2021-10-18 11:53:24 -0600

    updated

--- lesson5/js/preston.js
+++ lesson5/js/preston.js
@@ -7,7 +7,7 @@ var m = new Array ("January","February","March","April","May","June","July",
 "August","September","October","November","December");
 var d = new Array ("Sunday", "Monday", "Tuesday", "Wednesday", "Thursday",
 "Friday", "Saturday");
-var edited = d[u.getDay()] + ', ' + u.getDay() + ' ' + m[u.getMonth()] + ' ' + u.getFullYear();
+var edited = d[u.getDay()] + ', ' + u.getDate() + ' ' + m[u.getMonth()] + ' ' + u.getFullYear();
 document.getElementById("lastmodified").innerHTML = edited;
 
 var today = new Date();
