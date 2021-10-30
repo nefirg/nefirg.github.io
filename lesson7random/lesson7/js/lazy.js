fcb2cec7cd04d4c9d44ca064af724faa3d14f5eb
nefirg <gue15004@byui.edu>
2021-10-29 20:36:56 -0600

    update

--- lesson7random/lesson7/js/lazy.js
+++ lesson7random/lesson7/js/lazy.js
@@ -1,21 +1,3 @@
-function toggleMenu() {
-  document.getElementById("primaryNav").classList.toggle("hide");
-}
-
-var u = new Date(document.lastModified);
-var m = new Array ("January","February","March","April","May","June","July",
-"August","September","October","November","December");
-var d = new Array ("Sunday", "Monday", "Tuesday", "Wednesday", "Thursday",
-"Friday", "Saturday");
-var edited = d[u.getDay()] + ', ' + u.getDate() + ' ' + m[u.getMonth()] + ' ' + u.getFullYear();
-document.getElementById("lastmodified").innerHTML = edited;
-
-var today = new Date();
-var cday = today.getDay();
-if (cday != '5') {
-  document.getElementById('friday').style.display = "none";
-}
-
 let imagesToLoad = document.querySelectorAll("img[data-src]");
 
 const imgOptions = {
@@ -45,4 +27,22 @@ if ("IntersectionObserver" in window) {
   imagesToLoad.forEach((img) => {
     loadImages(img);
   });
+}
+
+function toggleMenu() {
+  document.getElementById("primaryNav").classList.toggle("hide");
+}
+
+var u = new Date(document.lastModified);
+var m = new Array ("January","February","March","April","May","June","July",
+"August","September","October","November","December");
+var d = new Array ("Sunday", "Monday", "Tuesday", "Wednesday", "Thursday",
+"Friday", "Saturday");
+var edited = d[u.getDay()] + ', ' + u.getDate() + ' ' + m[u.getMonth()] + ' ' + u.getFullYear();
+document.getElementById("lastmodified").innerHTML = edited;
+
+var today = new Date();
+var cday = today.getDay();
+if (cday != '5') {
+  document.getElementById('friday').style.display = "none";
 }
