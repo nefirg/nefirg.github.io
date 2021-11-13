030b540fef8b4d4ba7171e835d639c4d516e9c3d
nefirg <gue15004@byui.edu>
2021-11-13 15:07:51 -0700

    up

--- lesson09/js/index.js
+++ lesson09/js/index.js
@@ -1,3 +1,15 @@
+// function toggleMenu() {
+//     document.getElementById("primaryNav").classList.toggle("hide");
+//   }
+  
+//   var u = new Date(document.lastModified);
+//   var m = new Array ("January","February","March","April","May","June","July",
+//   "August","September","October","November","December");
+//   var d = new Array ("Sunday", "Monday", "Tuesday", "Wednesday", "Thursday",
+//   "Friday", "Saturday");
+//   var edited = d[u.getDay()] + ', ' + u.getDate() + ' ' + m[u.getMonth()] + ' ' + u.getFullYear();
+//   document.getElementById("lastmodified").innerHTML = edited;
+
 const requestURL = 'https://nefirg.github.io/lesson09/towndata.json';
 
 fetch(requestURL)
@@ -9,8 +21,8 @@ fetch(requestURL)
   })
   .then(function (jsonObject) {
     console.table(jsonObject); // temporary checking for valid response and data parsing
-    const prophets = jsonObject['towns'];  
-    for (let i = 0; i < prophets.length; i++ ) {
+    const towns = jsonObject['towns'];  
+    for (let i = 0; i < towns.length; i++ ) {
         let card = document.createElement('section');
 let h2 = document.createElement('h2');
 let h3 = document.createElement('h3');
@@ -26,11 +38,13 @@ p1.textContent = "Year Founded: " + towns[i].yearFounded;
 p2.textContent = "Population: " + towns[i].currentPopulation;
 p3.textContent = "Annual Rain Fall: " + towns[i].averageRainfall;
 image.setAttribute('src', towns[i].photo);
-image.setAttribute('alt', towns[i].name + ' ' + towns[i].lastname + ' - ' + towns[i].order);
+image.setAttribute('alt', towns[i].name + ' - ' + towns[i].motto + ' - ' + towns[i].yearFounded);
 
 card.appendChild(h2);
+card.appendChild(h3);
 card.appendChild(p1);
 card.appendChild(p2);
+card.appendChild(p3);
 card.appendChild(image);
 
 document.querySelector('div.cards').appendChild(card);
@@ -38,3 +52,4 @@ document.querySelector('div.cards').appendChild(card);
 .catch(function(error){
    console.log('Fetch error: ', error.message);
 })
+
