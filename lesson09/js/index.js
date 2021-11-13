9626bab8c6397b81becfaf93f9f6508558ce6512
nefirg <gue15004@byui.edu>
2021-11-13 14:13:56 -0700

    u

--- lesson09/js/index.js
+++ lesson09/js/index.js
@@ -16,14 +16,16 @@ let h2 = document.createElement('h2');
 let h3 = document.createElement('h3');
 let p1 = document.createElement('p');
 let p2 = document.createElement('p');
+let p3 = document.createElement('p');
 let image = document.createElement('img');
 
 
 h2.textContent = towns[i].name;
-h3.textContent = towns[i].motton;
-p1.textContent = "Date of Birth: " + towns[i].birthdate;
-p2.textContent = "Place of Birth: " + towns[i].birthplace;
-image.setAttribute('src', towns[i].imageurl);
+h3.textContent = towns[i].motto;
+p1.textContent = "Year Founded: " + towns[i].yearFounded;
+p2.textContent = "Population: " + towns[i].currentPopulation;
+p3.textContent = "Annual Rain Fall: " + towns[i].averageRainfall;
+image.setAttribute('src', towns[i].photo);
 image.setAttribute('alt', towns[i].name + ' ' + towns[i].lastname + ' - ' + towns[i].order);
 
 card.appendChild(h2);
